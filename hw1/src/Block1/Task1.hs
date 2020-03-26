889a19bcae8c9fec47fc4bf8c24e7a456feb2095
AnkaShch <shchetsova.au@yandex.ru>
2020-03-26 23:24:39 +0300

    fix codestyle in Block1.Task1

--- hw1/src/Block1/Task1.hs
+++ hw1/src/Block1/Task1.hs
@@ -1,29 +1,22 @@
 module Block1.Task1
-  ( Day(..)
+  (
+    Day (..)
   , afterDays
   , daysToParty
   , isWeekend
   , nextDay
   ) where
 
-data Day
-  = Monday
-  | Tuesday
-  | Wednesday
-  | Thursday
-  | Friday
-  | Saturday
-  | Sunday
-  deriving (Show)
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday deriving Show
 
 nextDay :: Day -> Day
-nextDay Monday = Tuesday
-nextDay Tuesday = Wednesday
+nextDay Monday    = Tuesday
+nextDay Tuesday   = Wednesday
 nextDay Wednesday = Thursday
-nextDay Thursday = Friday
-nextDay Friday = Saturday
-nextDay Saturday = Sunday
-nextDay Sunday = Monday
+nextDay Thursday  = Friday
+nextDay Friday    = Saturday
+nextDay Saturday  = Sunday
+nextDay Sunday    = Monday
 
 afterDays :: Day -> Integer -> Day
 afterDays day 0 = day
@@ -31,19 +24,19 @@ afterDays day n = afterDays (nextDay day) (n - 1)
 
 isWeekend :: Day -> Bool
 isWeekend Saturday = True
-isWeekend Sunday = True
-isWeekend _ = False
+isWeekend Sunday   = True
+isWeekend _        = False
 
 daysToParty :: Day -> Integer
 daysToParty Friday = 0
-daysToParty day = daysToParty (nextDay day) + 1
+daysToParty day    = daysToParty (nextDay day) + 1
 
 instance Eq Day where
-  Monday == Monday = True
-  Tuesday == Tuesday = True
+  Monday == Monday       = True
+  Tuesday == Tuesday     = True
   Wednesday == Wednesday = True
-  Thursday == Thursday = True
-  Friday == Friday = True
-  Saturday == Saturday = True
-  Sunday == Sunday = True
-  _ == _ = False
+  Thursday  == Thursday  = True
+  Friday == Friday       = True
+  Saturday == Saturday   = True
+  Sunday == Sunday       = True
+  _ == _                 = False
